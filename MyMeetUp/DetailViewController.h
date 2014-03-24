d923363ead4681dd2bc69b502a1774b783fdb550
jaime-h <jaime.e.hernandez@gmail.com>
2014-03-24 15:40:34 -0500

    Third Step

---
+++ MyMeetUp/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MyMeetUp
+//
+//  Created by Jaime Hernandez on 3/24/14.
+//  Copyright (c) 2014 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
